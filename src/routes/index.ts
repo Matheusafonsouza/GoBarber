230a61172d76eea93ee7521ac14082152c0f1fb8
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-25 00:17:07 -0300

    chore: Created routes config for appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,8 @@
 import { Router } from 'express';
+import appointmentRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/appointments', appointmentRouter);
 
 export default routes;
