dfc71e99a2f913419013bf32d960d8816b863bbc
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-25 00:25:31 -0300

    feature: Created appointments store method

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'This appointment is already booked.' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
