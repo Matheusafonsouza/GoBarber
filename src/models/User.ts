1308f0a12e5b26e022bc3b8fd04cdebc7df1a403
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-26 20:43:36 -0300

    fix: Added password column on user migration

---
+++ src/models/User.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
+
+export default User;
