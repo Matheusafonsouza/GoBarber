2a66991fb2e03ecef63c1934e9c30a106e0fdf05
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-27 20:56:39 -0300

    refactor: Updated provider to provider_id on appointmentsService

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -16,14 +16,14 @@ appointmentRouter.get('/', async (request, response) => {
 
 appointmentRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
 
     const appointment = await createAppointment.execute({
-      provider,
+      provider_id,
       date: parsedDate,
     });
 
