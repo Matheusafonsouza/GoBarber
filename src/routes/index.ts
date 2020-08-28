2249dd652aae401a17736f28cdac4b2276fa802e
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-28 09:42:20 -0300

    feature: Created session login method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+
 import appointmentRouter from './appointments.routes';
 import userRouter from './users.routes';
+import sessionRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
