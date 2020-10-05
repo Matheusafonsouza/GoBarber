31f4cd1bd24c9e38a102fcb0b68e2ef84dfb76bd
Matheus Afonso <matheusafonsouza@gmail.com>
2020-10-05 20:01:02 -0300

    feat: Created Provider to tmp storage

---
+++ src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import IStorageProvider from '../models/IStorageProvider';
+
+interface IStorageFile {
+  file: string;
+}
+
+class FakeStorageProvider implements IStorageProvider {
+  private storage: string[] = [];
+
+  public async saveFile(file: string): Promise<string> {
+    this.storage.push(file);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const findIndex = this.storage.findIndex(
+      storageFile => storageFile === file,
+    );
+
+    this.storage.splice(findIndex, 1);
+  }
+}
+
+export default FakeStorageProvider;
