1a29e9f73fb960283e356ffbd6c8027d64af7264
Matheus Afonso <matheusafonsouza@gmail.com>
2020-10-08 17:47:35 -0300

    feat: Created migration/repository for UserTokens

---
+++ src/modules/users/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,29 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
+
+import UserToken from '../entities/UserToken';
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({ user_id });
+
+    await this.ormRepository.save(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepository.findOne({ where: { token } });
+
+    return userToken;
+  }
+}
+
+export default UsersTokensRepository;
