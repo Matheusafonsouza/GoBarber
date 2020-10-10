5f16a7cba2571c5986cf09a742a55d4878636907
Matheus Afonso <matheusafonsouza@gmail.com>
2020-10-10 16:09:27 -0300

    feat: Created provider for email sending and reset password

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,11 +31,11 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    await this.usersTokensRepository.generate(user.id);
+    const { token } = await this.usersTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(
+    await this.mailProvider.sendMail(
       email,
-      'Pedido de recuperação de senha recebido!',
+      `Pedido de recuperação de senha recebido!: ${token}`,
     );
   }
 }
