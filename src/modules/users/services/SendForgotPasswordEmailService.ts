da8e13ff2d58ad65b2300ef93c3fafb315cd392c
Matheus Afonso <matheusafonsouza@gmail.com>
2020-10-10 16:40:09 -0300

    feat: Created mail templates with handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,8 +5,6 @@ import IMailProvider from '@shared/container/providers/MailProvider/models/IMail
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUsersTokensRepository from '../repositories/IUsersTokensRepository';
 
-// import User from '../infra/typeorm/entities/User';
-
 interface IRequest {
   email: string;
 }
@@ -33,10 +31,20 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.usersTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido!: ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
---
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -0,0 +1,18 @@
+import handlebars from 'handlebars';
+
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({
+    template,
+    variables,
+  }: IParseMailTemplateDTO): Promise<string> {
+    console.log('aaaaaaaaaaaaaaaa');
+    const parseTemplate = handlebars.compile(template);
+
+    return parseTemplate(variables);
+  }
+}
+
+export default HandlebarsMailTemplateProvider;
