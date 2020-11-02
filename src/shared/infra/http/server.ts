3e9da7b5306c1aaf42cada6382aef68b535e6718
Matheus Afonso <matheusafonsouza@gmail.com>
2020-11-02 14:29:01 -0300

    fix: fixing rate limiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,10 +16,10 @@ import '@shared/container/index';
 
 const app = express();
 
-app.use(RateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(RateLimiter);
 app.use(routes);
 
 app.use(errors());
